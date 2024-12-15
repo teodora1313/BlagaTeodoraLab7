3e16fadda355e5e1c91aa1957ca5da348a5704e6
YONDER\tblaga <teodora.blaga@tss-yonder.com>
2024-12-15 17:26:29 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace BlagaTeodoraLab7
+﻿using BlagaTeodoraLab7.Data;
+
+namespace BlagaTeodoraLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
---
+++ Models/ShopList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using MaxLengthAttribute = SQLite.MaxLengthAttribute;
+
+namespace BlagaTeodoraLab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
