3e34368237c649d5b05bab1315b80ace043c825e
YONDER\tblaga <teodora.blaga@tss-yonder.com>
2024-12-16 23:20:16 +0200

    Commit

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -1,5 +1,6 @@
 using BlagaTeodoraLab7.Models;
 using Microsoft.Maui.Devices.Sensors;
+using Plugin.LocalNotification;
 
 namespace BlagaTeodoraLab7;
 
@@ -26,6 +27,21 @@ public partial class ShopPage : ContentPage
         var location = locations?.FirstOrDefault();
         // var myLocation = await Geolocation.GetLocationAsync();
         var myLocation = new Location(46.7731796289, 23.6213886738);
+
+        var distance = myLocation.CalculateDistance(location, DistanceUnits.Kilometers);
+        if (distance < 4)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+            }; 
+            LocalNotificationCenter.Current.Show(request);
+        }
         await Map.OpenAsync(location, options);
     }
 }
